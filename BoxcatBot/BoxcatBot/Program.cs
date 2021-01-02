7d033f9146658f130ae626bf8a31f3bac0381c32
smkoontz1 <smkoontz1@gmail.com>
2021-01-01 19:29:45 -0600

    Initial bot setup with DSharpPlus.

---
+++ BoxcatBot/BoxcatBot/Program.cs
@@ -0,0 +1,37 @@
+﻿using DSharpPlus;
+using System.Threading.Tasks;
+
+namespace BoxcatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "",
+                TokenType = TokenType.Bot,
+            });
+
+            discord.MessageCreated += async (e) =>
+            {
+                var channel = e.Channel;
+                var messageContent = e.Message.Content.ToLower();
+
+                if (messageContent.StartsWith("ping"))
+                {
+                    await e.Message.RespondAsync("pong!");
+                }
+
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
